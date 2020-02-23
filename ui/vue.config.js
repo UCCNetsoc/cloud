6aab9dda505f447f5d1d108c6a7653c90782f285
Oisín Canty <git@ocanty.com>
2020-02-23 20:06:43 +0000

    ui: fixed vuetify bug and allowed for non localhost-only dev

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    // If you're developing on a VM
+    // This is required if you're visiting an npm run serve
+    // without a localhost URL
+    disableHostCheck: true
+  }
 }
