c37ab00377d7fca8011913079bf0e440470e138b
Oisín Canty <git@ocanty.com>
2020-05-03 20:27:55 +0100

    ui: enabled previously committed modules

--- ui/src/store/index.ts
+++ ui/src/store/index.ts
@@ -1,15 +1,29 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import Vuex, { ModuleTree } from 'vuex'
+
+import auth, { State as AuthState } from './auth'
+import oidcClientStorage, { State as OidcClientStorageState } from './storage'
+// import { User, UserManager } from 'oidc-client';
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+// Merge all the types of modules we use
+// This is done to provide typing
+// Vuex Typescript support bad
+interface RootState {
+  auth: AuthState;
+  oidcClientStorage: OidcClientStorageState;
+}
+
+const modules: ModuleTree<RootState> = {
+  auth,
+  oidcClientStorage
+}
+
+const store = new Vuex.Store<RootState>({
+  // The fields will be done by Vuex
+  state: ({ } as RootState),
+  modules
 })
+
+export default store
