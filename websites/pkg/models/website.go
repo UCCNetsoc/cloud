f5207e591522f65e8870b37a5775f9f42aa99de3
Oisín Canty <git@ocanty.com>
2020-05-05 20:30:06 +0100

    websites: website models

--- websites/pkg/models/website.go
+++ websites/pkg/models/website.go
@@ -1,13 +1,15 @@
-package websites
+package models
 
-// WebsiteUserOpts User specified options for a websites
-type WebsiteUserOpts struct {
-	Host string `json:"host"`
-	Path string `json:"path"`
+// Website represents a website with vhosts and root index directory
+type Website struct {
+	Hosts []string `json:"hosts"` // Array of virtual hosts that will serve this website
+	Root  string   `root:"root"`  // The root path of the website index
 }
 
-// Website Website
-type Website struct {
-	WebsiteUserOpts
-	User string `json:"user"`
+// InvalidWebsite represents a website that is invalid in some fashion
+// i.e the Hosts don't resolve, path doesn't exist, etc...
+type InvalidWebsite struct {
+	Hosts []string `json:"hosts"`
+	Root  string   `json:"root"`
+	Error error    `json:"error"`
 }
