e05e33167fea8e367fc7dce76cb727a0333d4053
Eric Moynihan <git@moynihan.io>
2020-09-09 18:20:48 +0100

    Added dev env to start-dev

--- start-dev.sh
+++ start-dev.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+if [[ $# -eq 0 ]] ; then
+    echo 'Must provide path to dev-env'
+    exit 0
+fi
+
 if [ ! -d "./bin" ]; then
 	virtualenv -p /usr/bin/python3 .
 	source bin/activate
-	pip install -r ./requirements.txt
+	pip3 install -r ./requirements.txt
+	cd $1/backing-services/freeipa
 	./freeipa-delete-data.sh
 	./freeipa-decompress-data.sh
-	cd ./ui && npm install && cd ..
-	
+	cd - && cd ./ui && npm install && cd ..
 fi
 
-bash --init-file <(echo "source bin/activate")
+cd $1
+bash --init-file <(echo "source bin/activate") -c "./dev-env up admin api"
