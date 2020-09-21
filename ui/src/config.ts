e6a8f5cdeae34bded89b3ac314055eb3db94b127
Eric Moynihan <git@moynihan.io>
2020-09-21 15:04:22 +0100

    env fix

--- ui/src/config.ts
+++ ui/src/config.ts
@@ -9,7 +9,7 @@ export const config: Config = {
   apiBaseUrl: process.env.VUE_APP_NETSOC_API_URL.replace(
     new RegExp('[/]+$'),
     ''
-  ) ?? 'http://api.netsoc.dev',
-  oidcAuthority: process.env.VUE_APP_OIDC_AUTHORITY ?? 'http://keycloak.netsoc.dev/auth/realms/freeipa',
-  hCaptchaSiteKey: process.env.NODE_ENV === 'development' ? (process.env.VUE_APP_HCAPTCHA_SITE_KEY ?? '') : '0e8176fb-1fc2-4d51-a153-773cbd8b9837'
+  ),
+  oidcAuthority: process.env.VUE_APP_OIDC_AUTHORITY,
+  hCaptchaSiteKey: process.env.VUE_APP_HCAPTCHA_SITE_KEY ?? ''
 }
