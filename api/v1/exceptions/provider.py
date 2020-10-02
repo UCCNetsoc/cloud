da4d2b765ce633770da352e0b508fe22a4817240
Oisín Canty <git@ocanty.com>
2020-10-02 22:48:35 +0100

    provider exception fixes

--- api/v1/exceptions/provider.py
+++ api/v1/exceptions/provider.py
@@ -1,26 +1,14 @@
 from .exception import APIException
 
 class ProviderException(APIException):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, reason):
+        super().__init__(reason)
 
 
 class Unavailable(ProviderException):
-    reason: str
-
     def __init__(self, reason):
-        super().__init__()
-        self.reason = reason
-
-    def __str__(self):
-        return f"Provider unavailable: {self.reason}"
+        super().__init__(f"Provider unavailable: {self.reason}")
 
 class Failed(ProviderException):
-    reason: str
-
     def __init__(self, reason):
-        super().__init__()
-        self.reason = reason
-
-    def __str__(self):
-        return f"Provider failed: {self.reason}"
+        super().__init__(f"Provider failed: {self.reason}")
