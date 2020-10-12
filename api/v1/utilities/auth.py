71df7580f4e5c31c63ca3ddc871e7d328023a569
Oisín Canty <git@ocanty.com>
2020-10-12 15:48:08 +0100

    added private key/public key generator

--- api/v1/utilities/auth.py
+++ api/v1/utilities/auth.py
@@ -4,7 +4,7 @@ import structlog
 from fastapi import Depends, Security
 from fastapi.security import OAuth2PasswordBearer, SecurityScopes
 from pydantic import ValidationError
-from typing import List
+from typing import List, Tuple
 
 from v1 import exceptions, models, providers
 from v1.config import config
@@ -92,3 +92,20 @@ def ensure_sysadmin(
         )
     )
     
+from cryptography.hazmat.primitives import serialization as crypto_serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend as crypto_default_backend
+
+def generate_rsa_public_private_key_pair(
+    key_size=2048
+) -> Tuple[str, str]:
+    key = rsa.generate_private_key(
+        backend=crypto_default_backend(),
+        public_exponent=65537,
+        key_size=key_size
+    )
+
+    return (
+        key.public_key().public_bytes(crypto_serialization.Encoding.OpenSSH, crypto_serialization.PublicFormat.OpenSSH)
+        key.private_bytes(crypto_serialization.Encoding.PEM, crypto_serialization.PrivateFormat.PKCS8, crypto_serialization.NoEncryption())
+    )
