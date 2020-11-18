ad6acf6a7f04d0c48b138967d423b57ad3b25642
Thomas007G <thomas007g@gmail.com>
2020-11-18 18:36:19 +0000

    added models for file upload

--- api/v1/models/filemanager.py
+++ api/v1/models/filemanager.py
@@ -1,17 +1,19 @@
 from typing import Optional, Union, List
 from pathlib import Path
-from pydantic import BaseModel, constr
+from pydantic import BaseModel
 
 from .account import Account
 from .jwt import Payload
 
 import time
 import os
+import paramiko
 
 class Stat(BaseModel):
-    isdir: bool
-    size: int
-    uid: int
-    gid: int
-    last_edited: int
-    perms: str
+    paramiko.SFTPAttributes
+
+class DirStat(BaseModel):
+    filename: os.stat_result
+
+class Dest(BaseModel):
+    path: str=Path(default="/")
