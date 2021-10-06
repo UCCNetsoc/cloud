75a4e092f95b81eb502dc88e2303cdc726fde0a6
Thomas Daniel Galligan <thomas007g@gmail.com>
2021-10-06 13:31:57 +0100

    fixed drawer issues, made site slightly more usable on mobile

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -6,7 +6,7 @@
       hide-overlay
       app
       clipped
-      style="background-color: rgba(37,37,37,0.8);"
+      style="background-color: rgba(37, 37, 37, 0.8)"
       mini-variant
       v-if="this.$store.state.auth.user !== null"
     >
@@ -24,20 +24,31 @@
             </v-list-item-action>
             <v-list-item-content>
               <v-list-item-title>{{ item.name }}</v-list-item-title>
-              <v-list-item-subtitle v-if="item.hasOwnProperty('subtitle')">{{ item.subtitle }}</v-list-item-subtitle>
+              <v-list-item-subtitle v-if="item.hasOwnProperty('subtitle')">{{
+                item.subtitle
+              }}</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
-
-    <v-app-bar app color="primary" dark clipped-left v-if="this.$store.state.auth.user !== null">
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" v-if="drawer == true">
-        <v-icon>mdi-close</v-icon>
-      </v-app-bar-nav-icon>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" v-else>
-        <v-icon>mdi-menu</v-icon>
-      </v-app-bar-nav-icon>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+      clipped-left
+      v-if="this.$store.state.auth.user !== null"
+    >
+      <div class="hamburger" style="width: " v-if="this.$vuetify.breakpoint.mdAndDown">
+        <v-app-bar-nav-icon
+          @click.stop="drawer = !drawer"
+          v-if="drawer == true"
+        />
+        <v-app-bar-nav-icon
+          @click.stop="drawer = !drawer"
+          v-else
+        />
+      </div>
       <v-toolbar-title class="ma-0">
         <div>
           <v-img
@@ -50,16 +61,18 @@
             width="120"
             height="42"
             @click="gotoLanding()"
-            style="cursor: pointer;"
+            style="cursor: pointer"
           />
         </div>
       </v-toolbar-title>
 
       <v-spacer></v-spacer>
-      <span style="cursor: pointer; margin-right: 0.125em" v-on:click="$router.push('/account')" class="font-weight-bold">
-        <v-icon class="align-center" size="24">
-          mdi-account-circle
-        </v-icon>
+      <span
+        style="cursor: pointer; margin-right: 0.125em"
+        v-on:click="$router.push('/account')"
+        class="font-weight-bold"
+      >
+        <v-icon class="align-center" size="24"> mdi-account-circle </v-icon>
         {{ this.$store.state.auth.user.profile.preferred_username }}
       </span>
       <v-btn text v-on:click="$router.push('/accounts/logout', () => {})">
@@ -72,21 +85,21 @@
       </v-slide-y-transition>
       <v-footer
         app
-        style="background-color: rgba(22,22,22,0.80)"
+        style="background-color: rgba(22, 22, 22, 0.8)"
         v-if="this.$store.state.auth.user !== null"
       >
         <small>
           &copy; 2021 UCC Networking Society
           <span> &bull; </span>
-          <a
-            target="_blank"
-            href="https://github.com/UCCNetsoc/cloud"
-          >Open Source Software</a>
+          <a target="_blank" href="https://github.com/UCCNetsoc/cloud"
+            >Open Source Software</a
+          >
           <span> &bull; </span>
           <a
             target="_blank"
             href="https://github.com/UCCNetsoc/wiki/blob/master/services/terms-of-service.md"
-          >Terms of Service</a>
+            >Terms of Service</a
+          >
         </small>
         <v-spacer></v-spacer>
         <a href="https://discord.netsoc.co" class="ml-1">
@@ -96,13 +109,17 @@
         <a
           href="https://github.com/UCCNetsoc"
           class="ml-1"
-          style="padding-bottom: 7px; display:block; border-bottom: 2px solid white"
+          style="
+            padding-bottom: 7px;
+            display: block;
+            border-bottom: 2px solid white;
+          "
         >
           <v-img
             width="24"
             height="24"
             src="@/assets/github.svg"
-            style="margin-bottom: -4px; margin-top: 4px;"
+            style="margin-bottom: -4px; margin-top: 4px"
           />
         </a>
         <a href="https://twitter.com/UCCNetsoc" class="ml-1">
@@ -202,12 +219,12 @@ export default Vue.extend({
       i++
     }
 
-    if (this.selectedMenu !== 0) {
-      this.drawer = true
-    } else {
-      // Hide drawer if we're on a non-menu page
-      this.drawer = false
-    }
+    // if (this.selectedMenu !== 0) {
+    //   this.drawer = true
+    // } else {
+    //   // Hide drawer if we're on a non-menu page
+    //   this.drawer = false
+    // }
   }
 })
 </script>
--- ui/src/views/Account.vue
+++ ui/src/views/Account.vue
@@ -1,36 +1,38 @@
 <template>
-  <v-container fluid>
-    <v-row>
-      <v-col cols="12" sm="6">
-        <v-card>
-          <v-card-title>
-            Hi {{ $store.state.auth.user.profile.preferred_username }}!
-          </v-card-title>
-          <v-card-subtitle>
-            UCC Netsoc welcomes you!
-          </v-card-subtitle>
-          <v-card-text class="ma-0 justify-center">
-            Username: <span class="white--text text--lighten-1">{{ $store.state.auth.user.profile.preferred_username }}</span><br/>
-            Email: <span class="white--text text--lighten-1">{{ $store.state.auth.user.profile.email }}</span><br/>
-            <br/>
-            <p>
-              Your account is used for logging into UCC Netsoc web services
+  <v-container fluid style="width: 100%; height: 100%;" class="d-flex flex-column align-center justify-center">
+    <!-- <v-row>
+      <v-col cols="12" sm="6"> -->
+        <v-card :style="this.$vuetify.smAndDown ? 'width: 100%' : 'width: 60%; padding: 2em;'">
+          <div :style="this.$vuetify.smAndDown ? 'width: 100%' : 'width: 60%; margin: auto; color: red'">
+            <v-card-title>
+              Hi {{ $store.state.auth.user.profile.preferred_username }}!
+            </v-card-title>
+            <v-card-subtitle>
+              UCC Netsoc welcomes you!
+            </v-card-subtitle>
+            <v-card-text class="ma-0 justify-center">
+              Username: <span class="white--text text--lighten-1">{{ $store.state.auth.user.profile.preferred_username }}</span><br/>
+              Email: <span class="white--text text--lighten-1">{{ $store.state.auth.user.profile.email }}</span><br/>
               <br/>
-              Please remember to keep the <b>Terms of Use</b> and its <b>Acceptable Use Policy</b> in mind as you share server hardware with our other users.
-            </p>
-            <v-btn color="warning" class="mx-1" @click="passwordReset.visible=true">
-              Reset Password
-            </v-btn>
-            <v-btn color="primary" class="mx-1" @click="gdprRequest.visible=true">
-              Data Request
-            </v-btn>
-            <v-btn color="red" class="mx-1" @click="deleteAccount.visible=true">
-              Delete Account
-            </v-btn>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+              <p>
+                Your account is used for logging into UCC Netsoc web services
+                <br/>
+                Please remember to keep the <b>Terms of Use</b> and its <b>Acceptable Use Policy</b> in mind as you share server hardware with our other users.
+              </p>
+              <v-btn color="warning" class="mx-1" @click="passwordReset.visible=true">
+                Reset Password
+              </v-btn>
+              <v-btn color="primary" class="mx-1" @click="gdprRequest.visible=true">
+                Data Request
+              </v-btn>
+              <v-btn color="red" class="mx-1" @click="deleteAccount.visible=true">
+                Delete Account
+              </v-btn>
+            </v-card-text>
+          </div>
+          </v-card>
+      <!-- </v-col>
+    </v-row> -->
     <password-reset-dialog :visible="passwordReset.visible" @successful="passwordReset.visible=false" @cancelled="passwordReset.visible=false"/>
     <g-d-p-r-dialog :visible="gdprRequest.visible" @successful="gdprRequest.visible=false" @cancelled="gdprRequest.visible=false"/>
     <delete-account-dialog :visible="deleteAccount.visible" @successful="deleteAccount.visible=false"/>
