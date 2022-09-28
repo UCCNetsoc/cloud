226445c9215b9af0e295532ebc16072f356129f6
Thomas Daniel Galligan <thomas007g@gmail.com>
2022-09-28 23:46:27 +0100

    switch apiBaseUrl fetch strategy, added hcaptcha sitekey

--- ui/src/config.ts
+++ ui/src/config.ts
@@ -7,12 +7,12 @@ export interface Config {
 }
 // TODO: change defaults to netsoc.co
 export const config: Config = {
-    apiBaseUrl: (process.env.REACT_APP_NETSOC_API_URL || "http://localhost:8000").replace(
+    apiBaseUrl: (process.env.REACT_APP_NETSOC_API_URL ?? "http://localhost:8000").replace(
         new RegExp('[/]+$'),
         ''
     ),
     oidcAuthority: process.env.REACT_APP_OIDC_AUTHORITY ?? "https://keycloak.netsoc.co/auth/realms/freeipa",
-    hCaptchaSiteKey: "10000000-ffff-ffff-ffff-000000000001",
+    hCaptchaSiteKey: "82ab2e7f-4765-4cff-ab41-648eb653ac11",
     sshUrl: process.env.REACT_APP_SSH_URL ?? '',
     sftpUrl: process.env.REACT_APP_SFTP_URL ?? ''
 }
